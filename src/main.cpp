69abfc3927e08902681144665b7b2435afe55ad4
Nika Tomanova <veronikatomanova470@gmail.com>
2026-09-12 09:37:35 +0300

    Update message in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 # include <iostream>
 
 int main() {
-    std::cout << "Hello, my name is Nika. I am from IU5-14/4!" << std::endl;
+    std::cout << "Updated message!" << std::endl;
     return 0;
 }
